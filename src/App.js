8983b1359d05001027fcdabfad284545fdab9991
Vishwejeet <vishwejeet.singh@superagi.com>
2025-10-23 00:47:18 +0530

    Fix routing for GitHub Pages - use HashRouter instead of BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { HeroView, WhatToExpectView, AboutUsView, ContactView, FooterView } from './views';
 import SurveyPage from './pages/SurveyPage';
 import Navbar from './components/Navbar';
